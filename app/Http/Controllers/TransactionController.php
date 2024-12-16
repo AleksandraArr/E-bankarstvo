01b2cc11291f834723e3ca9f550cdde427031304
Aleksandra Rakovic <aleksandra.rakovic21@gmail.com>
2024-12-16 01:30:31 +0100

    Izmenjena pretraga i dodati kriterijumi:datum, iznos i status

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -13,8 +13,12 @@ class TransactionController extends Controller
 {
     public function search(Request $request, $senderId)
     {
-        $query = Transaction::query();
+        $query = Transaction::where(function ($q) use ($senderId) {
+            $q->where('sender_account', $senderId)
+            ->orWhere('receiver_account', $senderId);
+        });
         $user = Auth::user();
+
         if (!$user) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
@@ -27,17 +31,37 @@ class TransactionController extends Controller
         if ($account->owner_id !== $user->id) {
             return response()->json(['message' => 'Not allowed'], 404);
         }
-        
-        $query->where('sender_account', $senderId)
-          ->orWhere('receiver_account', $senderId);
+
+        if ($request->has('category_id')) {
+            $query->where('category_id', '=', $request->query('category_id'));
+        }
 
         if ($request->has('receiver_account')) {
-            $query->where('receiver_account', $request->query('receiver_account'))
-                ->where('sender_account', $senderId);
+            if ($request->query('receiver_account') === $senderId) {
+                return response()->json(['message' => 'Cannot have same receiver and sender'], 404);
+            } else {
+                $query->where('receiver_account', $request->query('receiver_account'));
+            }
         }
-        
-        if ($request->has('category_id')) {
-            $query->where('category_id', $request->query('category_id'));
+
+        if ($request->has('amount_max')) {
+            $query->where('amount', '<=', $request->query('amount_max'));
+        }
+
+        if ($request->has('amount_min')) {
+            $query->where('amount', '>=', $request->query('amount_min'));
+        }
+
+        if ($request->has('date_from')) {
+            $query->where('date', '>=', $request->query('date_from'));
+        }
+
+        if ($request->has('date_to')) {
+            $query->where('date', '<=', $request->query('date_to'));
+        }
+
+        if ($request->has('status')) {
+            $query->where('status',  $request->query('status'));
         }
 
         $transactions = $query->get();
