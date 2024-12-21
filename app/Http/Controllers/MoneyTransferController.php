1ae2e696d00c7df6fac2fc1ec223b2d3164b9537
Milena Sretenovic <ms20210061@student.fon.bg.ac.rs>
2024-12-21 17:18:48 +0100

    Omogucen transfer izmedju deviznih i dinarskih racuna, implementiran api za dnevne kursne liste

--- app/Http/Controllers/MoneyTransferController.php
+++ app/Http/Controllers/MoneyTransferController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Account;
 use App\Models\Transaction;
+use App\Models\Currency;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\UserResource;
 use App\Http\Resources\AccountResource;
@@ -15,18 +16,19 @@ class MoneyTransferController extends Controller
 {
     public function transfer(Request $request, $sender_account_id)
     {
-        $user = Auth::user(); 
-        
-        $validated = Validator::make($request->all(), [
+        $user = Auth::user();
+        $validator = Validator::make($request->all(), [
             'receiver_account_number' => 'required|string|regex:/^[A-Z]{3}\d{10}$/',
             'amount' => 'required|numeric|min:0.01',
-            'description' => 'nullable|string|max:255',
-            'category_id' => 'nullable|exists:transaction_categories,id',
+            'description' => 'required|string|max:255',
+            'category_id' => 'required|exists:transaction_categories,id',
         ]);
     
-        if ($validated->fails()) {
-            return response()->json(['errors' => $validated->errors()], 422);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
         }
+    
+        $validated = $validator->validated();
 
         $sender = Account::findOrFail($sender_account_id);
 
@@ -34,20 +36,27 @@ class MoneyTransferController extends Controller
             return response()->json(['message' => 'Unauthorized'], 403);
         }
 
+        if ($sender->account_number === $validated['receiver_account_number']) {
+            return response()->json(['message' => 'Cannot transfer to the same account'], 400);
+        }
+
         if ($sender->balance < $validated['amount']) {
             return response()->json(['message' => 'Insufficient funds'], 400);
         }
 
-        $code = substr($validated['receiver_account_number'],0,3);
+        $receiver = Account::where('account_number', $validated['receiver_account_number'])->first();
+        $isExternal = is_null($receiver);
+        
+        if(!$isExternal && $sender->owner_id === $receiver->owner_id){
+            return $this->processExchangeTransfer($sender, $validated);
+        }
 
+        $code = substr($validated['receiver_account_number'],0,3);
         if($sender->currency->name !== $code){
             return response()->json(['message' => 'Currency mismatch'], 400);
         }
 
-
-        $receiver = Account::where('account_number', $validated['receiver_account_number'])->first();
-
-        $isExternal = is_null($receiver);
+       
         if ($isExternal) {
             return $this->processExternalTransfer($sender, $validated);
         } else {
@@ -75,6 +84,7 @@ class MoneyTransferController extends Controller
                 return Transaction::create([
                     'sender_account' => $sender->id,
                     'receiver_account' => $receiver->id,
+                    'receiver_account_number' => $receiver->account_number,
                     'amount' => $validated['amount'],
                     'date' => now(),
                     'description' => $validated['description'],
@@ -92,6 +102,59 @@ class MoneyTransferController extends Controller
         } catch (\Exception $e) {
             return response()->json(['message' => 'Transfer failed', 'error' => $e->getMessage()], 500);
         }
+    }
+
+    private function processExchangeTransfer($sender, $validated){
+        $receiver = Account::where('account_number', $validated['receiver_account_number'])->first();
+        if (!$receiver) {
+            return response()->json(['message' => 'Receiver account not found'], 404);
+        }
+
+        try {
+            $exchangeRate =  Currency::fetchExchangeRate($sender->currency->name, $receiver->currency->name, $validated['amount']);
+    
+            if (!$exchangeRate) {
+                return response()->json(['message' => 'Exchange rate not available'], 400);
+            }
+    
+            $convertedAmount = $exchangeRate['convertedAmount'];
+    
+            try {
+                $transaction = \DB::transaction(function () use ($sender, $receiver, $convertedAmount, $validated) {
+                    $sender->balance -= $validated['amount'];
+                    $sender->save();
+    
+                    
+                    $receiver->balance += $convertedAmount;
+                    $receiver->save();
+    
+                   
+                    return Transaction::create([
+                        'sender_account' => $sender->id,
+                        'receiver_account' => $receiver->id,
+                        'receiver_account_number' => $receiver->account_number,
+                        'amount' => $convertedAmount,
+                        'date' => now(),
+                        'description' => $validated['description'],
+                        'status' => 'successful',
+                        'category_id' => $validated['category_id'],
+                        'scope' => 'internal', 
+                    ]);
+                });
+    
+                return response()->json([
+                    'message' => 'Exchange transfer successful',
+                    'transaction' => new TransactionResource($transaction)
+                ], 200);
+    
+            } catch (\Exception $e) {
+                return response()->json(['message' => 'Exchange transfer failed', 'error' => $e->getMessage()], 500);
+            }
+    
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error fetching exchange rate', 'error' => $e->getMessage()], 500);
+        }
+        
     }
 
     private function processExternalTransfer($sender, $validated)
--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -3,16 +3,48 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Exception;
+use Illuminate\Support\Facades\Http;
 
 class Currency extends Model
 {
-    // use HasFactory;
-//izbrisati exhange 
     protected $fillable = ['name', 'date', 'exchange_rate'];
 
     public function accounts() : HasMany
     {
        return $this->hasMany(Account::class);
+    }
+
+    public static function fetchExchangeRate($fromCurrency, $toCurrency, $amount = 1)
+    {
+        $apiKey = '3d2f22000a644df627cbf86a29a91adc0db2550d';
+        $apiUrl = "https://api.getgeoapi.com/v2/currency/convert";
+
+        $response = Http::get($apiUrl, [
+            'api_key' => $apiKey,
+            'from' => $fromCurrency,
+            'to' => $toCurrency,
+            'amount' => $amount,
+            'format' => 'json'
+        ]);
+
+        if ($response->successful()) {
+            $data = $response->json();
+
+            if (isset($data['status']) && $data['status'] === 'success' && isset($data['rates'][$toCurrency])) {
+                $rate = $data['rates'][$toCurrency]['rate'];
+                $convertedAmount = $rate * $amount;
+
+                return [
+                    'rate' => $rate,
+                    'convertedAmount' => $convertedAmount
+                ];
+            } else {
+                throw new \Exception('Failed to fetch exchange rate.');
+            }
+        } else {
+            throw new \Exception('Failed to fetch exchange rate.');
+        }
     }
 
 }
